a4f8b25a110ad0a7f642ad5a2f97c70b88e3f985
kkyeongrokk <sisiko8@naver.com>
2023-05-08 09:20:14 -0700

    add state variables

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 /*----- constants -----*/
+reelImgs = [];
 
 /*----- state variables -----*/
+let accMoney;
+let betPerSpin;
+let reelImg;
+let highScore;
 
 /*----- cached elements  -----*/
 
 /*----- event listeners -----*/
 
 /*----- functions -----*/
+init();
+
+function init() {
+  accMoney = 0;
+  betPerSpin = 15;
+  highScore = 0;
+}
